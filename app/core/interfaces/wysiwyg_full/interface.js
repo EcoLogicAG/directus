4b7ce2d215b3ff273d82894343415fc76d949358
EcoLogic Server <server@wellengang.ch>
2018-03-27 11:00:51 +0200

    Added new version of the tinyMCE plugin

--- app/core/interfaces/wysiwyg_full/interface.js
+++ app/core/interfaces/wysiwyg_full/interface.js
@@ -126,7 +126,7 @@ define([
       }
 
       var options = {
-        plugins: 'table hr lists link image print pagebreak code insertdatetime media autoresize paste preview tinyimages',
+        plugins: 'table hr lists link image print pagebreak code insertdatetime media autoresize paste preview',
         selector: '#wysiwyg_' + this.options.name,
         branding: false,
         skin: 'directus',
